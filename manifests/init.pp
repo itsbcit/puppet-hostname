f35eef3ed55e016e9eca6c1d217a9e9b233c2149
Jesse Weisner <jesse_weisner@bcit.ca>
2018-05-08 15:47:16 -0700

    hostname method for centos-6

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,28 @@
 # @example
 #   include hostname
 class hostname {
+
   case $facts['os']['name'] {
-    default: { fail("Unsupported OS name: \'${facts['os']['name']}\'")}
+    'Alpine':
+      { $os_codename = "alpine-${facts['os']['release']['major']}.${facts['os']['release']['minor']}"}
+    'RedHat','CentOS','OracleLinux','Scientific':
+      { $os_codename = "el${facts['os']['release']['major']}" }
+    'Fedora':
+      { $os_codename = "fedora-${facts['os']['release']['major']}" }
+    'Ubuntu','Debian':
+      { $os_codename = $facts['os']['codename'] }
+    default:
+      { fail("Unsupported OS name: \'${facts['os']['name']}\'") }
+  }
+
+  if ( $::clientcert =~ /^localhost/ ) {
+    fail('Host name cannot be \"localhost\". Use --certname for Puppet client registration.')
+  }
+
+  # RedHat and CentOS 6 and earlier use a simple /etc/hostname file with no control program
+  if ( ($facts['os']['family'] == 'RedHat') and ($facts['os']['name'] != 'Fedora') ) {
+    if $facts['os']['release']['major'] <= '6' {
+      include ::hostname::method::file_exec
+    }
   }
 }
---
+++ manifests/method/file.pp
@@ -0,0 +1,23 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include hostname::method::file
+class hostname::method::file {
+
+  file { '/etc/hostname':
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0644',
+    content => "${::clientcert}\n",
+    notify  => Exec['set-hostname'],
+  }
+
+  exec { 'set-hostname':
+    command => '/bin/hostname -F /etc/hostname',
+    unless  => '/usr/bin/test `hostname` = `/bin/cat /etc/hostname`',
+  }
+
+}
