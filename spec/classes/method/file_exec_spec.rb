5d072a654da2e316cc449611c32c27a78da2efab
Jesse Weisner <jesse_weisner@bcit.ca>
2018-05-09 12:57:07 -0700

    renamed

--- spec/classes/method/file_exec_spec.rb
+++ spec/classes/method/file_exec_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'hostname::method::file' do
+describe 'hostname::method::file_exec' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
