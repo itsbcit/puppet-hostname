67daa1d1517546f47a8c004d53b0371f93c6c27e
Jesse Weisner <jesse_weisner@bcit.ca>
2018-05-09 12:57:21 -0700

    added hostnamectl method

---
+++ manifests/method/hostnamectl.pp
@@ -0,0 +1,12 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include hostname::method::hostnamectl
+class hostname::method::hostnamectl {
+  exec { 'set-hostname':
+    command => "/usr/bin/hostnamectl set-hostname ${::clientcert}",
+    unless  => '/usr/bin/test `hostname` = `/bin/cat /etc/hostname`',
+  }
+}
